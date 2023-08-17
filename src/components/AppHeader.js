7bcc22de5b7836dbefc91abaa6b030af3fb5061e
Kristine <chankristineagcay@gmmail.com>
2023-08-17 23:11:31 +0800

    added button

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Button from './Button';
 
 function AppHeader() {
   return (
     <div>
       <h1> Hello from header </h1>
+      <Button> Click Me</Button>
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,24 @@
+import React, { Children } from 'react';
+import styles from '../styles/modules/button.module.scss';
+import { getClasses } from '../utils/getClasses';
+
+const buttonTypes = {
+  primary: 'primary',
+  secondary: 'secondary',
+};
+
+function Button({ children, variant = 'primary' }) {
+  return (
+    <button
+      className={getClasses([
+        styles.button,
+        styles[`button--${buttonTypes[variant]}`], // Use styles instead of style
+      ])}
+      type="button"
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
