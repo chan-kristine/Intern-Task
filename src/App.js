f286c6d280bea22c89811971cb4c467e3b2934f3
Kristine <chankristineagcay@gmmail.com>
2023-08-17 22:09:08 +0800

    Adding Task Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PageTitle from './components/PageTitle';
+import AppHeader from './components/AppHeader';
+import style from './styles/modules/app.module.scss';
 
 function App() {
   return (
     <div className="container">
       <PageTitle>TO-DO LIST</PageTitle>
+      <div className={style.app__wrapper}>
+        <AppHeader />
+      </div>
     </div>
   );
 }
