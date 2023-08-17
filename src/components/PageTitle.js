177f05c96139e9f4ae8b0f541b64f77482845f7c
Kristine <chankristineagcay@gmmail.com>
2023-08-17 20:42:57 +0800

    Done Title Layout

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,11 +1,8 @@
 import React from 'react';
+import style from '../styles/modules/title.module.scss';
 
 function PageTitle({ children }) {
-  return (
-    <div className="container">
-      <h1>{children}</h1>
-    </div>
-  );
+  return <p className={style.title}>{children}</p>;
 }
 
 export default PageTitle;
