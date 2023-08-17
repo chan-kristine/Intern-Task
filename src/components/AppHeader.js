c1966853c0a3d8cf65b013c4b03bf58343815989
Kristine <chankristineagcay@gmmail.com>
2023-08-17 23:59:33 +0800

    Added options for user (Incomplete & Comple Task)

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import Button from './Button';
+import { Button, SelectButton } from './Button';
 
 function AppHeader() {
   return (
     <div>
       <h1> Hello from header </h1>
       <Button variant="primary">Click Me</Button>
+      <SelectButton>
+        <option value="all">ALL</option>
+        <option value="incomplete">incomplete</option>
+        <option value="complete">Complete</option>
+      </SelectButton>
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,4 @@
-import React, { Children } from 'react';
+import React from 'react';
 import styles from '../styles/modules/button.module.scss';
 import { getClasses } from '../utils/getClasses';
 
@@ -7,12 +7,12 @@ const buttonTypes = {
   secondary: 'secondary',
 };
 
-function Button({ children, type, variant, ...rest }) {
+function Button({ children, type = 'button', variant = 'primary', ...rest }) {
   return (
     <button
       className={getClasses([
         styles.button,
-        styles[`button--${buttonTypes[variant]}`], // Use styles instead of style
+        styles[`button--${buttonTypes[variant]}`],
       ])}
       type={type === 'submit' ? 'submit' : 'button'}
       {...rest}
@@ -22,4 +22,15 @@ function Button({ children, type, variant, ...rest }) {
   );
 }
 
-export default Button;
+function SelectButton({ children, ...rest }) {
+  return (
+    <select
+      className={getClasses([styles.button, styles.button__select])}
+      {...rest}
+    >
+      {children}
+    </select>
+  );
+}
+
+export { Button, SelectButton };
