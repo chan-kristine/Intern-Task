713b682f7f904763aeef591a51f058c3fba5a0f9
Kristine <chankristineagcay@gmmail.com>
2023-08-18 21:32:09 +0800

    edit button and done comple update

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,50 +1,63 @@
-import React, { useState } from 'react';
 import { format } from 'date-fns';
+import { motion } from 'framer-motion';
+import toast from 'react-hot-toast';
+import React, { useEffect, useState } from 'react';
 import { MdDelete, MdEdit } from 'react-icons/md';
 import { useDispatch } from 'react-redux';
 import { deleteTodo, updateTodo } from '../slices/todoSlice';
 import styles from '../styles/modules/todoItem.module.scss';
+import { getClasses } from '../utils/getClasses';
+import CheckButton from './CheckButton';
 import TodoModal from './TodoModal';
+
+const child = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
 
 function TodoItem({ todo }) {
   const dispatch = useDispatch();
+  const [checked, setChecked] = useState(false);
   const [updateModalOpen, setUpdateModalOpen] = useState(false);
 
-  const handleCheckboxChange = () => {
-    const updatedStatus =
-      todo.status === 'complete' ? 'incomplete' : 'complete';
+  useEffect(() => {
+    if (todo.status === 'complete') {
+      setChecked(true);
+    } else {
+      setChecked(false);
+    }
+  }, [todo.status]);
+
+  const handleCheck = () => {
+    setChecked(!checked);
     dispatch(
-      updateTodo({ id: todo.id, status: updatedStatus, title: todo.title })
+      updateTodo({ ...todo, status: checked ? 'incomplete' : 'complete' })
     );
   };
 
   const handleDelete = () => {
     dispatch(deleteTodo(todo.id));
+    toast.success('Todo Deleted Successfully');
   };
 
   const handleUpdate = () => {
     setUpdateModalOpen(true);
   };
 
-  const handleCloseModal = () => {
-    setUpdateModalOpen(false);
-  };
-
   return (
     <>
-      <div className={styles.item}>
+      <motion.div className={styles.item} variants={child}>
         <div className={styles.todoDetails}>
-          <input
-            type="checkbox"
-            checked={todo.status === 'complete'}
-            onChange={handleCheckboxChange}
-          />
+          <CheckButton checked={checked} handleCheck={handleCheck} />
           <div className={styles.texts}>
             <p
-              className={[
+              className={getClasses([
                 styles.todoText,
-                todo.status === 'complete' && styles['todoText--complete'],
-              ].join(' ')}
+                todo.status === 'complete' && styles['todoText--completed'],
+              ])}
             >
               {todo.title}
             </p>
@@ -56,31 +69,29 @@ function TodoItem({ todo }) {
         <div className={styles.todoActions}>
           <div
             className={styles.icon}
-            onClick={handleDelete}
-            onKeyDown={handleDelete}
-            role="button"
+            onClick={() => handleDelete()}
+            onKeyDown={() => handleDelete()}
             tabIndex={0}
+            role="button"
           >
             <MdDelete />
           </div>
           <div
             className={styles.icon}
-            onClick={handleUpdate}
-            onKeyDown={handleUpdate}
-            role="button"
+            onClick={() => handleUpdate()}
+            onKeyDown={() => handleUpdate()}
             tabIndex={0}
+            role="button"
           >
             <MdEdit />
           </div>
         </div>
-      </div>
+      </motion.div>
       <TodoModal
         type="update"
-        todo={todo}
         modalOpen={updateModalOpen}
         setModalOpen={setUpdateModalOpen}
-        onClose={handleCloseModal}
-        status={todo.status}
+        todo={todo}
       />
     </>
   );
