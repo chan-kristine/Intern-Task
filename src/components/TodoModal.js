489965bc7712f8d431cac16a85851d42ec574fe4
Kristine <chankristineagcay@gmmail.com>
2023-08-18 09:45:37 +0800

    fix functionality using other approach

--- src/components/TodoModal.js
+++ src/components/TodoModal.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MdClose } from 'react-icons/md';
 import styles from '../styles/modules/modal.module.scss';
 import { Button } from './Button';
 
 function TodoModal({ modalOpen, setModalOpen }) {
+  const [title, setTitle] = useState('');
+  const [status, setStatus] = useState('');
+
   return (
     modalOpen && (
       <div className={styles.wrapper}>
@@ -25,11 +28,21 @@ function TodoModal({ modalOpen, setModalOpen }) {
             <h1 className={styles.formTitle}>Add Task</h1>
             <label htmlFor="title">
               Title
-              <input type="text" id="title" />
+              <input
+                type="text"
+                id="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
             </label>
             <label htmlFor="status">
               Status
-              <select name="status" id="status">
+              <select
+                name="status"
+                id="status"
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
+              >
                 <option value="incomplete">Incomplete</option>
                 <option value="complete">Complete</option>
               </select>
