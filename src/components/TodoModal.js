508b48f322cd2e9d0edb855e16236f8fd47d9656
Kristine <chankristineagcay@gmmail.com>
2023-08-18 09:30:04 +0800

    adjust layout

--- src/components/TodoModal.js
+++ src/components/TodoModal.js
@@ -5,58 +5,56 @@ import { Button } from './Button';
 
 function TodoModal({ modalOpen, setModalOpen }) {
   return (
-    <div>
-      {modalOpen && (
-        <div className={styles.wrapper}>
-          <div className={styles.container}>
-            <div
-              className={styles.closeButton}
-              onClick={() => setModalOpen(false)}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  setModalOpen(false);
-                }
-              }}
-              tabIndex={0}
-              role="button"
-            >
-              <MdClose />
-            </div>
-            <form className={styles.form}>
-              <h1 className={styles.formTitle}>Add Task</h1>
-              <label htmlFor="title">
-                Title
-                <input type="text" id="title" />
-              </label>
-              <label htmlFor="status">
-                Status
-                <select name="status" id="status">
-                  <option value="incomplete">Incomplete</option>
-                  <option value="complete">Complete</option>
-                </select>
-              </label>
-              <div className={styles.buttonContainer}>
-                <Button type="submit" variant="primary">
-                  Add Task
-                </Button>
-                <Button
-                  type="button"
-                  variant="secondary"
-                  onClick={() => setModalOpen(false)}
-                  onKeyDown={(e) => {
-                    if (e.key === 'Enter') {
-                      setModalOpen(false);
-                    }
-                  }}
-                >
-                  Cancel
-                </Button>
-              </div>
-            </form>
+    modalOpen && (
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <div
+            className={styles.closeButton}
+            onClick={() => setModalOpen(false)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                setModalOpen(false);
+              }
+            }}
+            tabIndex={0}
+            role="button"
+          >
+            <MdClose />
           </div>
+          <form className={styles.form}>
+            <h1 className={styles.formTitle}>Add Task</h1>
+            <label htmlFor="title">
+              Title
+              <input type="text" id="title" />
+            </label>
+            <label htmlFor="status">
+              Status
+              <select name="status" id="status">
+                <option value="incomplete">Incomplete</option>
+                <option value="complete">Complete</option>
+              </select>
+            </label>
+            <div className={styles.buttonContainer}>
+              <Button type="submit" variant="primary">
+                Add Task
+              </Button>
+              <Button
+                type="button"
+                variant="secondary"
+                onClick={() => setModalOpen(false)}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    setModalOpen(false);
+                  }
+                }}
+              >
+                Cancel
+              </Button>
+            </div>
+          </form>
         </div>
-      )}
-    </div>
+      </div>
+    )
   );
 }
 
