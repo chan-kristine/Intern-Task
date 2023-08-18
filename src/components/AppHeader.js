f60516dcb7bde6264bf8696690b05d9598ba853f
Kristine <chankristineagcay@gmmail.com>
2023-08-18 09:12:50 +0800

    add tas and cancel function

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, SelectButton } from './Button';
 import styles from '../styles/modules/app.module.scss';
 import TodoModal from './TodoModal';
 
 function AppHeader() {
+  const [modalOpen, setModalOpen] = useState(false); // Corrected usage
+
   return (
     <div className={styles.appHeader}>
-      <Button variant="primary">Add Task</Button>
+      {/* Set the onClick handler to open the modal */}
+      <Button variant="primary" onClick={() => setModalOpen(true)}>
+        Add Task
+      </Button>
       <SelectButton id="status">
         <option value="all">ALL</option>
         <option value="incomplete">incomplete</option>
         <option value="complete">Complete</option>
       </SelectButton>
-      <TodoModal />
+      <TodoModal modalOpen={modalOpen} setModalOpen={setModalOpen} />{' '}
+      {/* Pass the props */}
     </div>
   );
 }
--- src/components/TodoModal.js
+++ src/components/TodoModal.js
@@ -3,36 +3,52 @@ import { MdClose } from 'react-icons/md';
 import styles from '../styles/modules/modal.module.scss';
 import { Button } from './Button';
 
-function TodoModal() {
+function TodoModal({ modalOpen, setModalOpen }) {
+  const handleAddTask = () => {
+    setModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setModalOpen(false);
+  };
+
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <div className={styles.closeButton}>
-          <MdClose />
-        </div>
-        <form className={styles.form}>
-          <h1 className={styles.formTitle}>Add Task</h1>
-          <label htmlFor="title">
-            Title
-            <input type="text" id="title" />
-          </label>
-          <label htmlFor="status">
-            Status
-            <select name="status" id="status">
-              <option value="incomplete">Incomplete</option>
-              <option value="complete">Complete</option>
-            </select>
-          </label>
-          <div className={styles.buttonContainer}>
-            <Button type="submit" variant="primary">
-              Add Task {}
-            </Button>
-            <Button type="button" variant="secondary">
-              Cancel {}
-            </Button>
+    <div>
+      {modalOpen && (
+        <div className={styles.wrapper}>
+          <div className={styles.container}>
+            <div className={styles.closeButton}>
+              <MdClose />
+            </div>
+            <form className={styles.form}>
+              <h1 className={styles.formTitle}>Add Task</h1>
+              <label htmlFor="title">
+                Title
+                <input type="text" id="title" />
+              </label>
+              <label htmlFor="status">
+                Status
+                <select name="status" id="status">
+                  <option value="incomplete">Incomplete</option>
+                  <option value="complete">Complete</option>
+                </select>
+              </label>
+              <div className={styles.buttonContainer}>
+                <Button type="button" variant="primary" onClick={handleAddTask}>
+                  Add Task
+                </Button>
+                <Button
+                  type="button"
+                  variant="secondary"
+                  onClick={handleCancel}
+                >
+                  Cancel
+                </Button>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
+        </div>
+      )}
     </div>
   );
 }
