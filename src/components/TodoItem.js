da8f537b740873f31612115a810e9de30e6d5fd1
Kristine <chankristineagcay@gmmail.com>
2023-08-18 12:31:05 +0800

    added delete and edit task

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { format } from 'date-fns';
+import { MdDelete, MdEdit } from 'react-icons/md';
 import styles from '../styles/modules/todoItem.module.scss';
 import { getClasses } from '../utils/getClasses';
 
@@ -19,6 +20,14 @@ function TodoItem({ todo }) {
           <p className={styles.time}>
             {format(new Date(todo.time), 'p, MM/dd/yyyy')}
           </p>
+        </div>
+      </div>
+      <div className={styles.todoActions}>
+        <div className={styles.icon}>
+          <MdDelete />
+        </div>
+        <div className={styles.icon}>
+          <MdEdit />
         </div>
       </div>
     </div>
